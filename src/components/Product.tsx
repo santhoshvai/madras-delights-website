8c8f6fe50a7ec3cfbb91dd5ceb23c94f8b5a63c9
Santhosh Vaiyapuri <santhoshvai@gmail.com>
2023-09-18 15:50:44 +0200

    feat: add zooming animation

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,9 +1,12 @@
+"use client";
+
 import * as React from "react";
 import Image from "next/image";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 
 export default function Product({ product }: { product: Product }) {
+  const [hover, setHover] = React.useState(false);
   return (
     <Box
       sx={{
@@ -11,18 +14,27 @@ export default function Product({ product }: { product: Product }) {
         maxWidth: "100%",
       }}
     >
-      <Image
-        src={require("../../public/" + product.image)}
-        alt={product.name}
-        style={{
-          margin: "auto",
-          display: "block",
-          maxWidth: "100%",
-          height: "auto",
+      <Box
+        sx={{
+          overflow: "hidden",
         }}
-        priority
-      />
-
+      >
+        <Image
+          src={require("../../../public/" + product.image)}
+          alt={product.name}
+          onMouseOver={() => setHover(true)}
+          onMouseOut={() => setHover(false)}
+          style={{
+            margin: "auto",
+            display: "block",
+            maxWidth: "100%",
+            height: "auto",
+            transition: "transform .5s ease",
+            transform: hover ? "scale(1.5)" : "scale(1)",
+          }}
+          priority
+        />
+      </Box>
       <Typography variant="h5" sx={{ textAlign: "center" }}>
         {product.name}
       </Typography>
