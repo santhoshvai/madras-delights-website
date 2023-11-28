81d0f9d56799e9409a4dcae6f9a469940d67955f
Santhosh Vaiyapuri <santhoshvai@gmail.com>
2023-11-28 17:32:22 +0100

    chore: remove the unnecessary dot

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,3 +1,3 @@
 export const TITLE = "Madras Delights";
 export const DESCRIPTION =
-  "At Madras Delights, we take immense pride in being the authorized distributor of Aswins sweets and snacks in the Netherlands.";
+  "At Madras Delights, we take immense pride in being the authorized distributor of Aswins sweets and snacks in the Netherlands";
