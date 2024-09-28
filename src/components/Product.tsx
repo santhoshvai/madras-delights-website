2033bb2aba87f75e4064019d7925ebd0427c06e9
Santhosh Vaiyapuri <santhoshvai@gmail.com>
2024-09-28 22:32:09 +0200

    feat: add tooltip and adjusted hover styles

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -2,53 +2,69 @@
 
 import * as React from "react";
 import Image from "next/image";
+import { styled } from "@mui/material/styles";
+import Tooltip, { TooltipProps, tooltipClasses } from "@mui/material/Tooltip";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 
 const getScale = (hover: boolean, product: Product) => {
-  let scale = hover ? 2.8 : 1.8;
+  let scale = hover ? 2.4 : 1.4;
   if (product.imageScale) {
     scale = hover ? product.imageScale.hover : product.imageScale.normal;
   }
   return "scale(" + scale + ")";
 };
 
+const LightTooltip = styled(({ className, ...props }: TooltipProps) => (
+  <Tooltip {...props} classes={{ popper: className }} />
+))(({ theme }) => ({
+  [`& .${tooltipClasses.tooltip}`]: {
+    backgroundColor: theme.palette.common.white,
+    color: "rgba(0, 0, 0, 0.87)",
+    boxShadow: theme.shadows[1],
+    fontSize: theme.typography.pxToRem(16),
+    border: "1px solid #dadde9",
+  },
+}));
+
 export default function Product({ product }: { product: Product }) {
   const [hover, setHover] = React.useState(false);
   return (
-    <Box
-      sx={{
-        margin: "auto",
-        maxWidth: "100%",
-      }}
-    >
+    <LightTooltip title={product.description}>
       <Box
         sx={{
-          overflow: "hidden",
-          borderRadius: "10px",
+          margin: "auto",
+          maxWidth: "100%",
         }}
       >
-        <Image
-          src={`/${product.image}`}
-          alt={product.name}
-          width={442}
-          height={663}
-          onMouseOver={() => setHover(true)}
-          onMouseOut={() => setHover(false)}
-          style={{
-            margin: "auto",
-            display: "block",
-            maxWidth: "100%",
-            height: "auto",
-            transition: "transform .5s ease",
-            transform: getScale(hover, product),
+        <Box
+          sx={{
+            overflow: "hidden",
+            borderRadius: "10px",
           }}
-          priority
-        />
+        >
+          <Image
+            src={`/${product.image}`}
+            alt={product.name}
+            width={442}
+            height={663}
+            onMouseOver={() => setHover(true)}
+            onMouseOut={() => setHover(false)}
+            style={{
+              margin: "auto",
+              display: "block",
+              maxWidth: "100%",
+              height: "auto",
+              transition: "transform .5s ease",
+              transform: getScale(hover, product),
+            }}
+            priority
+          />
+        </Box>
+        <Typography variant="h5" sx={{ textAlign: "center" }}>
+          {product.name}
+        </Typography>
       </Box>
-      <Typography variant="h5" sx={{ textAlign: "center" }}>
-        {product.name}
-      </Typography>
-    </Box>
+    </LightTooltip>
   );
 }
